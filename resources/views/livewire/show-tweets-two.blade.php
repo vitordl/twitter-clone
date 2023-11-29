3ea609d0f4c05016556df85af3c55b82e682cd47
Vitor Notebook Secundario <vitortactic@gmail.com>
2023-11-29 17:50:19 -0300

    att

--- resources/views/livewire/show-tweets-two.blade.php
+++ resources/views/livewire/show-tweets-two.blade.php
@@ -39,7 +39,7 @@
                         <button type="submit"
                         class="mb-1 bg-blue-500 py-2 px-5 rounded-full text-white hover:bg-blue-600 "
                         >
-                        Post</button>
+                        <strong>Post</strong></button>
                     </div>
 
                     <div class="text-red-400 mb-6">
@@ -57,10 +57,10 @@
           <div>
             @foreach ($tweets as $tweet)
         
-            <a href="#" class="hover:bg-gray-100 flex">
+            <a href="#" class="hover:bg-gray-200 flex px-2">
 
         
-              <div class="px-2  mt-8 text-sm sm:text-base">
+              <div class="mt-8 text-sm sm:text-base">
                 
                   <div class="flex gap-x-2">
                       <div class="shrink-0">
@@ -92,7 +92,7 @@
                   <div class="px-6 ">
                     <div class="flex px-6 mt-2 ">
                                           
-                        <a href="#" class="fill-gray-500 hover:fill-blue-500 "><svg viewBox="0 0 24 24" aria-hidden="true" class="w-5 h-5 "><g><path d="M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6c-3.317 0-6.005 2.69-6.005 6 0 3.37 2.77 6.08 6.138 6.01l.351-.01h1.761v2.3l5.087-2.81c1.951-1.08 3.163-3.13 3.163-5.36 0-3.39-2.744-6.13-6.129-6.13H9.756z"></path></g></svg></a>
+                        <a href="#" class="fill-gray-500 hover:fill-blue-500"><svg viewBox="0 0 24 24" aria-hidden="true" class="w-5 h-5 "><g><path d="M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6c-3.317 0-6.005 2.69-6.005 6 0 3.37 2.77 6.08 6.138 6.01l.351-.01h1.761v2.3l5.087-2.81c1.951-1.08 3.163-3.13 3.163-5.36 0-3.39-2.744-6.13-6.129-6.13H9.756z"></path></g></svg></a>
                                           
                         <a href="#" class="fill-gray-500 hover:fill-green-500 px-8 md:px-16"><svg viewBox="0 0 24 24" aria-hidden="true" class="w-5 h-5 "><g><path d="M4.5 3.88l4.432 4.14-1.364 1.46L5.5 7.55V16c0 1.1.896 2 2 2H13v2H7.5c-2.209 0-4-1.79-4-4V7.55L1.432 9.48.068 8.02 4.5 3.88zM16.5 6H11V4h5.5c2.209 0 4 1.79 4 4v8.45l2.068-1.93 1.364 1.46-4.432 4.14-4.432-4.14 1.364-1.46 2.068 1.93V8c0-1.1-.896-2-2-2z"></path></g></svg></a>
                                           
